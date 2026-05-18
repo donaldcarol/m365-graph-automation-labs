f231c0cb8e84d0edb0e73a8db8f2bd2c0e491cf2
Donald Carol Fitai <donald@dakini.network>
2026-05-18 17:42:04 +0300

    Enhance script to install required Graph modules
    Added installation for required Microsoft Graph modules and updated connection scopes.

--- scripts/01-connect-interactive.ps1
+++ scripts/01-connect-interactive.ps1
@@ -8,17 +8,29 @@ It is intended for labs, testing and manual administration.
 #>
 
 # Install Microsoft Graph module if missing
-if (-not (Get-Module Microsoft.Graph -ListAvailable)) {
-    Install-Module Microsoft.Graph -Scope CurrentUser -Force
+$requiredModules = @(
+    "Microsoft.Graph.Authentication",
+    "Microsoft.Graph.Users",
+    "Microsoft.Graph.Groups",
+    "Microsoft.Graph.Identity.DirectoryManagement",
+    "Microsoft.Graph.DeviceManagement"
+)
+
+foreach ($module in $requiredModules) {
+    if (-not (Get-Module $module -ListAvailable)) {
+        Install-Module $module -Scope CurrentUser -Force -AllowClobber
+    }
 }
 
-Import-Module Microsoft.Graph
+Import-Module Microsoft.Graph.Authentication
 
 # Connect with common read-only scopes
 Connect-MgGraph -Scopes `
     "User.Read.All", `
     "Group.Read.All", `
-    "Device.Read.All"
+    "Device.Read.All", `
+    "DeviceManagementManagedDevices.Read.All" `
+    -NoWelcome
 
 # Display current Graph context
 Get-MgContext
