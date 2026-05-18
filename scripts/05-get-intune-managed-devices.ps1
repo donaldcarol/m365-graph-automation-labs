ee7bf65f14efa8994707351ca2aef928387097bd
Donald Carol Fitai <donald@dakini.network>
2026-05-18 17:13:22 +0300

    Add script to get Intune managed devices
    This script retrieves Intune managed devices using Microsoft Graph and selects specific properties for output.

---
+++ scripts/05-get-intune-managed-devices.ps1
@@ -0,0 +1,29 @@
+<#
+.SYNOPSIS
+Gets Intune managed devices using Microsoft Graph.
+
+.REQUIRED PERMISSION
+DeviceManagementManagedDevices.Read.All
+#>
+
+$devices = Get-MgDeviceManagementManagedDevice -All -Property `
+    Id,
+    DeviceName,
+    UserPrincipalName,
+    OperatingSystem,
+    OsVersion,
+    ComplianceState,
+    ManagementAgent,
+    EnrolledDateTime,
+    LastSyncDateTime
+
+$devices |
+Select-Object `
+    DeviceName,
+    UserPrincipalName,
+    OperatingSystem,
+    OsVersion,
+    ComplianceState,
+    ManagementAgent,
+    EnrolledDateTime,
+    LastSyncDateTime
